c8b3e1362d4b5c6af0bdb9bc566809651128f8d4
adeli <adeli@192.168.0.10>
2022-05-24 01:17:55 +0400

    First commit
    My commit

---
+++ ProjectGitHub/src/main/java/my/self/demo/ProjectGitHubApplication.java
@@ -0,0 +1,13 @@
+package my.self.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectGitHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectGitHubApplication.class, args);
+	}
+
+}
---
+++ ProjectGitHub/src/test/java/my/self/demo/ProjectGitHubApplicationTests.java
@@ -0,0 +1,13 @@
+package my.self.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectGitHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
